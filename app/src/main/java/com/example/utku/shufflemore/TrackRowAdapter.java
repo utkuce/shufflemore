cf98366f602bfeedad2475d4893e8c47ac6c26ea
utku <utkuce@gmail.com>
2018-05-24 12:23:38 +0300

    Add click listener to rows

--- app/src/main/java/com/example/utku/shufflemore/TrackRowAdapter.java
+++ app/src/main/java/com/example/utku/shufflemore/TrackRowAdapter.java
@@ -16,50 +16,57 @@ public class TrackRowAdapter extends RecyclerView.Adapter<TrackRowAdapter.ViewHo
 
     private List<Song> chosenSongs;
 
-        TrackRowAdapter(List<Song> chosenSongs) {
-            this.chosenSongs = chosenSongs;
-        }
+    TrackRowAdapter(List<Song> chosenSongs) {
+        this.chosenSongs = chosenSongs;
+    }
 
-        class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-            ImageView coverArt;
-            TextView trackInfo;
+        ImageView coverArt;
+        TextView trackInfo;
 
-            ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
 
-                super(itemView);
+            super(itemView);
 
-                coverArt = itemView.findViewById(R.id.cover_art);
-                trackInfo = itemView.findViewById(R.id.track_info);
-            }
-        }
+            coverArt = itemView.findViewById(R.id.cover_art);
+            trackInfo = itemView.findViewById(R.id.track_info);
 
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-            Context context = parent.getContext();
-            LayoutInflater inflater = LayoutInflater.from(context);
-
-            View contactView = inflater.inflate(R.layout.recycler_row, parent, false);
-
-            return new ViewHolder(contactView);
+            itemView.setOnClickListener(this);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull TrackRowAdapter.ViewHolder holder, int position) {
-
-            Song song = chosenSongs.get(position);
-
-            TextView textView = holder.trackInfo;
-            textView.setText(String.format("%s\n%s", song.name, song.artist));
-
-            ImageView imageView = holder.coverArt;
-            imageView.setImageBitmap(song.cover);
-        }
-
-        @Override
-        public int getItemCount() {
-            return chosenSongs.size();
+        public void onClick(View v) {
+            System.out.println("clicked" + getAdapterPosition());
         }
     }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View contactView = inflater.inflate(R.layout.recycler_row, parent, false);
+
+        return new ViewHolder(contactView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TrackRowAdapter.ViewHolder holder, int position) {
+
+        Song song = chosenSongs.get(position);
+
+        TextView textView = holder.trackInfo;
+        textView.setText(String.format("%s\n%s", song.name, song.artist));
+
+        ImageView imageView = holder.coverArt;
+        imageView.setImageBitmap(song.cover);
+    }
+
+    @Override
+    public int getItemCount() {
+            return chosenSongs.size();
+        }
+}
